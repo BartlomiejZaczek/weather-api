9de1f83e21f8ee5106bebe537fec5cabcfd0802e
BartlomiejZaczek <bartlomiej.zaczek.contact@gmail.com>
2023-12-20 14:21:41 +0100

    create controller

---
+++ src/main/java/com/example/weatherapi/controller/WindsurfingController.java
@@ -0,0 +1,21 @@
+package com.example.weatherapi.controller;
+
+import com.example.weatherapi.model.WindsurfingLocation;
+import com.example.weatherapi.service.WindsurfingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.FileNotFoundException;
+
+@RestController
+@RequiredArgsConstructor
+public class WindsurfingController {
+
+    private final WindsurfingService windsurfingService;
+    @GetMapping("/location")
+    WindsurfingLocation getWindsurfingLocation(@RequestParam String date) throws FileNotFoundException {
+        return windsurfingService.getWindsurfingLocation(date);
+    }
+}
