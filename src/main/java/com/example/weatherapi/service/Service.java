9f18dc40a4877785296ec9c3639b9a42e98863e2
BartlomiejZaczek <bartlomiej.zaczek.contact@gmail.com>
2023-12-20 12:52:09 +0100

    add city weather details fetching method

--- src/main/java/com/example/weatherapi/service/Service.java
+++ src/main/java/com/example/weatherapi/service/Service.java
@@ -1,7 +1,10 @@
 package com.example.weatherapi.service;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
 
 @org.springframework.stereotype.Service
 public class Service {
@@ -12,5 +15,13 @@ public class Service {
 
     public Service(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
+    }
+
+    /** Retrieve location data from URL and return as ResponseEntity of Map type */
+    public Map<String, Object> getLocationWeatherDetails(String city) {
+        ResponseEntity<Map> response = restTemplate.getForEntity(
+                URL + city + "&key=" + API_KEY,
+                Map.class);
+        return response.getBody();
     }
 }
