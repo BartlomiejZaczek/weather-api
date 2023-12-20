97c2c59ba21c7fdab3189e97031e10c7295d4379
BartlomiejZaczek <bartlomiej.zaczek.contact@gmail.com>
2023-12-20 12:28:45 +0100

    add configuration and api key env

---
+++ src/main/java/com/example/weatherapi/Config.java
@@ -0,0 +1,16 @@
+package com.example.weatherapi;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+public class Config {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
